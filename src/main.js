22861daa39d0bfab5a4d352fdba547eb1e8e9b7e
erer-c <1799684417@qq.com>
2020-01-07 21:45:11 +0800

    初始化完成

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './utils/register-vant'
+import 'normalize.css'
+import './styles/index.less'
+import 'amfe-flexible'// 配置rem适配-动态设置基准值
 
 Vue.config.productionTip = false
 
---
+++ src/utils/register-vant.js
@@ -0,0 +1,4 @@
+// 注册引用vant组件
+import Vue from 'vue'
+import { Button } from 'vant'
+Vue.use(Button)
