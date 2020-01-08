d271e34bd19542cb2b69c6e8a5de11033bd24792
erer-c <1799684417@qq.com>
2020-01-08 10:25:19 +0800

    登录页面-基本登录功能实现及登录提示

--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -1,8 +1,9 @@
 // 注册引用vant组件
 import Vue from 'vue'
-import { Button, NavBar, Field, Cell, CellGroup } from 'vant'
+import { Button, NavBar, Field, Cell, CellGroup, Toast } from 'vant'
 Vue.use(Button)
   .use(NavBar)
   .use(Field)
   .use(Cell)
   .use(CellGroup)
+  .use(Toast)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,24 +2,51 @@
   <div class="login">
     <van-nav-bar title="登录" />
     <van-cell-group>
-      <van-field label="用户名" placeholder="请输入用户名" />
-      <van-field label="验证码" placeholder="获取验证码" />
+      <van-field v-model="user.mobile" label="手机号" placeholder="请输入手机号" required />
+      <van-field v-model="user.code" label="验证码" placeholder="请输入验证码" required>
+        <van-button slot="button" type="primary" size="small">获取验证码</van-button>
+      </van-field>
     </van-cell-group>
     <div class="loginBtn">
-      <van-button type="info">登录</van-button>
+      <van-button type="info" @click="login">登录</van-button>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { login } from '@/api/user-login'
+export default {
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$toast.loading({
+        duration: 0, // 持续展示
+        message: '登录中...',
+        forbidClick: true
+      })
+      login(this.user).then(res => {
+        // alert(1)
+        this.$toast.success('登录成功！')
+      }).catch(() => {
+        this.$toast.fail('手机号或验证码错误！')
+      })
+    }
+  }
+}
 </script>
 
 <style scoped lang='less'>
-.loginBtn{
-    padding: 20px;
-    .van-button {
-        width: 100%;
-    }
+.loginBtn {
+  padding: 20px;
+  .van-button {
+    width: 100%;
+  }
 }
 </style>
